e386b4d7e20ca60911acef9bc2035c9fc53a0e64
ujjwal72486 <ujjwal72486@gmail.com>
2023-12-17 18:23:35 +0000

    hello world app

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,20 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(Text('Hello World', textDirection: TextDirection.rtl));
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  
+  const MyApp({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+  home: Scaffold(
+  body: Center(
+   child: Text('Hello World'),
+  )  ,
+),
+    );
+    // return const Text('Hello World', textDirection: TextDirection.ltr);
+  }
 }
